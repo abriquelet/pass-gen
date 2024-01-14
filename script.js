f4700c1d430c8470bb4e311b5d77f9ee3057724c
Khyber <ajbriquelet@gmail.com>
2024-01-13 18:10:42 -0600

    added and texted if statements that push data into my storage array

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ let passOptions = {
   number: "1234567890",
   upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lower: "abcdefghijklmnopqrstuvwxyz",
-  specChar: "!@#$%^&*()?.<\>|=+:;,[-_]",
-  // Data needs to be called from in here and sent into the storage array to be manipulated further down.
-  storage:[]
+  specChar: "!@#$%^&*()?.<\>|=+:;,[-_]"
 }
 // Below lies the function that runs when the button is clicked.
 let generatePassword = function() {
@@ -27,9 +25,30 @@ let generatePassword = function() {
   if (numberBool == false && upperBool == false && lowerBool == false && specBool == false) {
     window.alert("Error: Your password must contain at least one character type.")
   }
-  // Find way to call upon the actual data that needs to be mixed as opposed to boolean values.
-  // Reference the variables within the function(the ones with prompts that are booleans) to confirm types used.
-
+   // UPDATE: Storage moved into function to be referenced.
+   let storage = []; 
+  // IF booleanvalue = true, then ADD data type associated with boolean name to STORAGE using +=.
+  // pass.Options.category is used to reference the values within.
+  if (numberBool = true) {
+    storage += passOptions.number;
+    console.log(storage);
+    // expected output: contents of number var. 
+  }
+  if (upperBool = true) {
+    storage += passOptions.upper;
+    console.log(storage);
+    //expected output: string associated with var upper behind other vars if user selected. 
+  }
+  if (lowerBool = true) {
+    storage += passOptions.lower;
+    console.log(storage);
+    //expected output: string associated with var lower behind any other var strings if selected.
+  }
+  if (specBool = true) {
+    storage += passOptions.specChar;
+    console.log(storage);
+    // expected output: string associated with var specChar behind any other strings the user selected.
+  }
 }
 
 
