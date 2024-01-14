883262184e656057c9690bab94b457f574aaf9d1
Khyber <ajbriquelet@gmail.com>
2024-01-13 18:28:38 -0600

    fixed if statement logic to detect exactly equal to ensure unchosen data types are not included

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ let generatePassword = function() {
   let upperBool = window.confirm("Include uppercase letters? (OK to include, cancel to proceed to next question).");
   let lowerBool = window.confirm("Include lowercase letters? (OK to include, cancel to proceed to next question).");
   let specBool = window.confirm("Include special characters? (OK to include, cancel to proceed to next question).");
-  // added a console.log to ensure data was getting logged correctly after user input.
-  // Currently they're mostly booleans, must be turned into something to reference allowable data above by ref parent.
+  //console.log to ensure booleans logged correctly
   console.log(charAmount, numberBool, upperBool, lowerBool, specBool);
   // alerts user that their desired length does not meet requirements.
   if (charAmount <8 || charAmount >128) {
@@ -28,23 +27,23 @@ let generatePassword = function() {
    // UPDATE: Storage moved into function to be referenced.
    let storage = []; 
   // IF booleanvalue = true, then ADD data type associated with boolean name to STORAGE using +=.
-  // pass.Options.category is used to reference the values within.
-  if (numberBool = true) {
+  // pass.Options.category is used to reference the values within
+  if (numberBool === true) {
     storage += passOptions.number;
     console.log(storage);
     // expected output: contents of number var. 
-  }
-  if (upperBool = true) {
+  } 
+  if (upperBool === true) {
     storage += passOptions.upper;
     console.log(storage);
     //expected output: string associated with var upper behind other vars if user selected. 
   }
-  if (lowerBool = true) {
+  if (lowerBool === true) {
     storage += passOptions.lower;
     console.log(storage);
     //expected output: string associated with var lower behind any other var strings if selected.
   }
-  if (specBool = true) {
+  if (specBool === true) {
     storage += passOptions.specChar;
     console.log(storage);
     // expected output: string associated with var specChar behind any other strings the user selected.
